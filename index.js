0f55e5a85b888fa7a68af4ad1be8a6e44a8ef5d3
Kyle <khartson2017@gmail.com>
2021-10-14 14:30:38 -0400

    Completed assingment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+const num1 = 31; 
+const num2 = 2; 
+let multiply = num1*num2; 
+const random = Math.floor(Math.random() * 10) + 1;   
+const num3 = 42; 
+const num4 = 3; 
+let mod = (num3/num4) % 10; 
+let max = Math.max(1, 2, 4, 10 , 20); 
